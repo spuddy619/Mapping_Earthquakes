30b08a31279c08a6fd7dfc0ca90f9c3a7947b12e
Jun <junyoungdchun@gmail.com>
2022-02-09 00:10:00 -0800

    update .gitignore + D1 done

--- Earthquake_Challenge/static/js/logic_steps/logicStep1.js
+++ Earthquake_Challenge/static/js/logic_steps/logicStep1.js
@@ -13,8 +13,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
-    }
-);
+});
 
 
 // Create a base layer that holds both maps.
@@ -24,8 +23,8 @@ let baseMaps = {
 };
 
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 }) 
 
@@ -43,31 +42,26 @@ let myStyle = {
 }
 
 // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-    //also establishes color, fillColor, weight - with "myStyle" variable above
-    L.geoJSON(data, {
-        style: myStyle,
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME);
-        }      
-    }).addTo(map);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    L.geoJSON(data).addTo(map);
 });
 
 /////////////////
 /////////////////
 /////////////////
 
+// //Add console.log to see if code is functioning 
+// console.log("The code is working player");
+
 // // We create the dark view tile layer that will be an option for our map.
-// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
 //     accessToken: API_KEY
 // });
 
 // // We create the tile layer that will be the background of our map - specifically "light-v10" layer
-// let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 //     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 //     maxZoom: 18,
 //     accessToken: API_KEY
@@ -77,41 +71,40 @@ d3.json(torontoHoods).then(function(data) {
 
 // // Create a base layer that holds both maps.
 // let baseMaps = {
-//     "Night Navigation" : dark,
-//     "Day Navigation" : light
-
+//     "Streets" : streets,
+//     "Satellite Streets" : satelliteStreets
 // };
 
 // let map = L.map('mapid', {
-//     center: [44.0, -80.0],
-//     zoom: 2,
-//     layers: [dark]
+//     center: [43.7, -79.3],
+//     zoom: 11,
+//     layers: [streets]
 // }) 
 
 // // Pass our map layers into our layers control and add the layers control to the map.
 // L.control.layers(baseMaps).addTo(map);
 
-// // Accessing the Toronto airline routes GeoJSON URL
-// let torontoData = "https://raw.githubusercontent.com/spuddy619/Mapping_Earthquakes/main/torontoRoutes.json";
+// // Accessing the Toronto neighborhoods GeoJSON URL
+// let torontoHoods = "https://raw.githubusercontent.com/spuddy619/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // // Create a style for the lines- lines from "torontoData" json
 // let myStyle = {
-//     color: "#ffffa1", //yellow
-//     weight: 2
+//     color: "blue",
+//     fillColor: "yellow",
+//     weight: 1,
 // }
 
 // // Grabbing our GeoJSON data.
-// d3.json(torontoData).then(function(data) {
+// d3.json(torontoHoods).then(function(data) {
 //     console.log(data);
 // // Creating a GeoJSON layer with the retrieved data.
-// L.geoJSON(data, {
-//     style: myStyle,
-//     onEachFeature: function(feature, layer) {
-//         layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination: "
-//         + feature.properties.dst + "</h3>");
-//     }
-// })
-// .addTo(map);
+//     //also establishes color, fillColor, weight - with "myStyle" variable above
+//     L.geoJSON(data, {
+//         style: myStyle,
+//         onEachFeature: function(feature, layer) {
+//             layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME);
+//         }      
+//     }).addTo(map);
 // });
 
 
--- Mapping_Multiple_Points/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// Mapbox API key
-const API_KEY = "pk.eyJ1IjoianVkY2h1biIsImEiOiJja3lrcXBhNjIwNGF5MnBtZjBnOWRsa3I5In0.9FlMVjAoaqndjyHXTsJmxQ";
